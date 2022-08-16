caa5d38ff91265b63a6994bf9f6dedbc56c7f18b
Sergey Petrinich <petrinichshadow@gmail.com>
2022-08-16 17:07:00 +0300

    project updated to v3 of FLS template

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,25 +1,22 @@
-
 /*
 Документация по работе в шаблоне: https://www.lightgalleryjs.com/docs/
 Документация плагина: https://www.lightgalleryjs.com/docs/
 Сниппет(HTML):
 */
-
 // Подключение функционала "Чертогов Фрилансера"
 import { isMobile, FLS } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
 // Подключение базового набора функционала
-import lightGallery from 'lightgallery';
+import lightGallery from "lightgallery";
 
 // Плагины
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
 // import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
 //import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
-
 // Базовые стили
-import '@scss/libs/gallery/lightgallery.scss';
+import "@scss/libs/gallery/lightgallery.scss";
 // Стили дополнений
 // import '@scss/libs/gallery/lg-thumbnail.scss';
 // import '@scss/libs/gallery/lg-video.scss';
@@ -37,24 +34,20 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
-const galleries = document.querySelectorAll('[data-gallery]');
+const galleries = document.querySelectorAll("[data-gallery]");
+
 if (galleries.length) {
-	let galleyItems = [];
-	galleries.forEach(gallery => {
-		galleyItems.push({
-			gallery,
-			galleryClass: lightGallery(gallery, {
-				// plugins: [lgZoom, lgThumbnail],
-				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
-				speed: 500,
-			})
-		})
-	});
-	// Добавляем в объект модулей
-	flsModules.gallery = galleyItems;
+    let galleyItems = [];
+    galleries.forEach((gallery) => {
+        galleyItems.push({
+            gallery,
+            galleryClass: lightGallery(gallery, {
+                // plugins: [lgZoom, lgThumbnail],
+                licenseKey: "7EC452A9-0CFD441C-BD984C7C-17C8456E",
+                speed: 500,
+            }),
+        });
+    });
+    // Добавляем в объект модулей
+    flsModules.gallery = galleyItems;
 }
-
-
-
-
-
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,176 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+window.onload = function () {
+    if (document.querySelector(".body__content")) {
+        const blogItem = [
+            {
+                link: "#1",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "Tourism",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/01.jpg",
+            },
+            {
+                link: "#2",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "sport",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/02.jpg",
+            },
+            {
+                link: "#3",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "fashion",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/03.jpg",
+            },
+            {
+                link: "#4",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "clothes",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/04.jpg",
+            },
+            {
+                link: "#5",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "clothes",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/05.jpg",
+            },
+            {
+                link: "#6",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "fashion",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/06.jpg",
+            },
+            {
+                link: "#7",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "Summer",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/07.jpg",
+            },
+            {
+                link: "#8",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "Autumn",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/08.jpg",
+            },
+            {
+                link: "#9",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "clothes",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/09.jpg",
+            },
+            {
+                link: "#10",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "Summer",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/10.jpg",
+            },
+            {
+                link: "#11",
+                title: "One of Saturn’s largest rings may be newer than anyone",
+                category: "Summer",
+                date: "June 6, 2019",
+                author: "Rickie Baroch",
+                image: "img/content/11.jpg",
+            },
+        ];
 
+        let body = document.querySelector(".body__content");
+        blogItem.map((item, i) => {
+            let article = document.createElement("article");
+            article.className = `body__item blog-item ${i === 6 ? "_full" : ""}`;
+            article.innerHTML = `
+                <a href=${item?.link} class="blog-item__image-ibg">
+                    <img src=${item?.image} alt=${item?.title} />
+                </a>
+                <div class="blog-item__body">
+                    <a href=${item?.link} class="blog-item__category">${item?.category}</a>
+                    <h3 class="blog-item__title">
+                        <a href=${item?.link}>${item?.title}</a>
+                    </h3>
+                    <div class="blog-item__bottom">
+                        <a href=${item?.link} class="blog-item__date">${item?.date}</a>
+                        <a href=${item?.link} class="blog-item__name">
+                            <span>By</span>
+                            ${item?.author}
+                        </a>
+                    </div>
+                </div>
+            `;
+            body.append(article);
+        });
+    }
+
+    if (document.querySelector(".pagination-fill")) {
+        // selecting required element
+        const element = document.querySelector(".pagination-fill ul");
+        let totalPages = 8;
+        let page = 2;
+        element.innerHTML = createPagination(totalPages, page);
+        function createPagination(totalPages, page) {
+            let liTag = "";
+            let active;
+            let beforePage = page - 1;
+            let afterPage = page + 1;
+            if (page > 1) {
+                liTag += `<li class="pagination-fill__prev _icon-down"><span>OLDER POST</span></li>`;
+            }
+
+            if (page > 2) {
+                liTag += `<li class="pagination-fill__number pagination-fill__number_first"><span>1</span></li>`;
+                if (page > 3) {
+                    liTag += `<li class="pagination-fill__dots"><span>...</span></li>`;
+                }
+            }
+
+            // how many pages or li show before the current li
+
+            for (var plength = beforePage; plength <= afterPage; plength++) {
+                if (plength > totalPages) {
+                    continue;
+                }
+                if (plength == 0) {
+                    plength = plength + 1;
+                }
+                if (page == plength) {
+                    active = "_active";
+                } else {
+                    active = "";
+                }
+                liTag += `<li class="pagination-fill__number ${active}"><span>${plength}</span></li>`;
+            }
+
+            if (page < totalPages - 1) {
+                if (page < totalPages - 2) {
+                    liTag += `<li class="pagination-fill__dots"><span>...</span></li>`;
+                }
+                liTag += `<li class="pagination-fill__number pagination-fill__number_last"><span>${totalPages}</span></li>`;
+            }
+
+            if (page < totalPages) {
+                liTag += `<li class="pagination-fill__next _icon-down"><span>NEXT POST</span></li>`;
+            }
+            element.innerHTML = liTag;
+            return liTag;
+        }
+    }
+};
